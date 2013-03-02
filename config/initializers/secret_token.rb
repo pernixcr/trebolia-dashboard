82d28168d7594d7d0ca45ceb8cf85c7022e333b1
Esteban Hernandez <jehehe1@gmail.com>
2013-03-01 18:05:52 -0600

    Set up devise

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+TreboliaDashboard::Application.config.secret_token = '53251d9a8fe5fbaae31265f36eeec0ed0807e5d0dfa6e31deb792805a0a69134e2cb4ec652aae1419ea6abd563c4e6ebc4e64dc0dd9454dae7193965a20774d8'
