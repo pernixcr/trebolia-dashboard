e51886b9eea67ec4e451d02144108698719ed9c0
Esteban Hernandez <jehehe1@gmail.com>
2013-03-12 08:57:31 -0600

    Change database from Mysql to Sqllite3

--- app/models/registro_factura.rb
+++
@@ -1,4 +0,0 @@
-class RegistroFactura < ActiveRecord::Base
-    self.table_name = 'Registro_Facturas'
-    self.primary_key = :Id_Facturas
-end
--- test/unit/helpers/facturas_helper_test.rb
+++
@@ -1,4 +0,0 @@
-require 'test_helper'
-
-class FacturasHelperTest < ActionView::TestCase
-end
